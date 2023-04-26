42430021ff6541d36574f974db0664cfab25cfbf
Nicholas Brandt <nicholas.brandt@inf.ethz.ch>
2023-04-26 14:37:09 +0200

    fix create.sh

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -1,3 +1,17 @@
+# fix docker issue
+cd /var/lib/dpkg/info
+rm usrmerge.*
+
+# update submodules
+git submodule update --init --recursive
+
+# update system
+apt-get update
+apt-get install qpdf wget -y
+apt-get -y upgrade
+apt-get -y autoclean
+apt-get -y autoremove
+
 # fix openssl to fetch remote bib files
 # https://stackoverflow.com/questions/72133316/libssl-so-1-1-cannot-open-shared-object-file-no-such-file-or-directory
 mkdir temp
@@ -6,15 +20,3 @@ wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1
 dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
 cd ..
 rm -r temp
-
-# fix docker issue
-cd /var/lib/dpkg/info
-rm usrmerge.*
-
-git submodule update --init --recursive
-
-apt-get update
-apt-get install qpdf -y
-apt-get -y upgrade
-apt-get -y autoclean
-apt-get -y autoremove
